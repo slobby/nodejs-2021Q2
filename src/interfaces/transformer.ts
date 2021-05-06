b9ebd013f1b8482e3d9021c3ec283bb80988ab3f
Andrew <andslobby@gmail.com>
2021-05-06 15:13:55 +0300

    fix: change algoritm of "getCode" function

--- src/interfaces/transformer.ts
+++ src/interfaces/transformer.ts
@@ -6,11 +6,13 @@ import encrypt from "../utils/encrypt"
 export class Transformer extends Transform
 {
    private _shift: number;
+   private _action: string; 
     
    constructor(opt = {}, shift: number, action: string)
    {
       super(opt);
-      this._shift = shift
+      this._shift = shift;
+      this._action = action;
    }
    /**
     * метод, реализующий в себе запись данных (chunk поступают в поток Transform), 
@@ -22,7 +24,7 @@ export class Transformer extends Transform
     */
    _transform(data : Buffer, encoding : any, callback : any)
   {
-      this.push(Buffer.from(encrypt(data.toString(), this._shift)));
+      this.push(Buffer.from(encrypt(data.toString(), this._shift, this._action)));
       callback();
    }
 }
--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,19 +1,23 @@
-export default function encrypt(text: string, shift: number): string {
+function getCode(startCode: number, endCode: number, charCode: number, shift: number, action: string) : number {
+    shift = (action === "encode") ? shift : -shift;
+    const alphaLength = Math.abs(endCode - startCode) + 1;
+    const calculatedCode : number = (charCode + shift - startCode) % alphaLength;
+    return (calculatedCode >= 0) ? (startCode + calculatedCode) : (endCode + 1 + calculatedCode);
+}
+
+export default function encrypt(text: string, shift: number, action: string): string {
     let result: string = "";
 
     for (let i = 0; i < text.length; i++) {
         const char: string = text[i];
         const charCode: number = char.charCodeAt(0);
         if (charCode >= 65 && charCode <= 90) {
-            result += String.fromCodePoint((charCode + shift - 65) % 26 + 65);
+            result += String.fromCodePoint(getCode(65, 90, charCode, shift, action));
         }
         else if (charCode >= 97 && charCode <= 122) {
-            result += String.fromCodePoint((charCode + shift - 97) % 26 + 97);
+            result += String.fromCodePoint(getCode(97, 122, charCode, shift, action));
         }
-        else {
-            result += char;
+        else { result += char; }
         }
-        }
-        return result;
-    }
-
+    return result;
+}
